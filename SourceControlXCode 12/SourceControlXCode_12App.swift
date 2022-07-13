ce4e9a61436dfb1a9f305cae5cc19f474e8b9c12
Mahaveer Gill <m.gill42@hotmail.com>
2022-07-13 17:09:51 +0100

    Initial Commit

---
+++ SourceControlXCode 12/SourceControlXCode_12App.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlXCode_12App.swift
+//  SourceControlXCode 12
+//
+//  Created by Mahaveer Gill on 13/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlXCode_12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
